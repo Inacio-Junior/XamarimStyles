d9bc0434a6533f91b1b6816ef729240a40b5207a
Inacio-Junior <inaciojunior10@yahoo.com.br>
2016-12-12 12:10:30 -0200

    Upload

---
+++ LandManager/LandManager/Model/Login.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LandManager.Model
+{
+    public class Login
+    {
+        public long NSerie { get; set; }
+
+        public string Email { get; set; }
+
+
+        public bool Logar(long nserie, string email)
+        {
+            bool vOk = false;
+
+            return vOk;
+        }
+    }
+}
---
+++ LandManager/LandManager/Views/viewLogin.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+
+namespace LandManager.Views
+{
+    public partial class viewLogin : ContentPage    
+    {                
+        public viewLogin()
+        {            
+            InitializeComponent();
+            //Padrão MVVM
+            //Atrela o code behind desta view a classe Login da pasta ViewsModel.            
+            this.BindingContext = new ViewsModel.vmLogin();
+
+            switchLogar.IsToggled = true;
+
+        }
+
+        private void SwitchLogar_OnToggled(object sender, ToggledEventArgs e)
+        {
+            if (e.Value == true)
+            {
+                lblCheck.Text = "Sim";
+            }
+            else
+            {
+                lblCheck.Text = "Não";
+            }
+        }
+    }
+}
